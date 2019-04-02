8bae1e810e68bcf484cc6f37f9484fc5b7a84016
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-04-02 21:58:57 +0200

    start of bac reminder logic

---
+++ class-bac-reminder.php
@@ -0,0 +1,18 @@
+<?php
+
+class BacReminder {
+    public function __construct() {
+        
+    }
+
+    public function init() {
+
+    }
+
+    // resend email
+
+    // order_comment_if_resend
+
+    
+}
+
