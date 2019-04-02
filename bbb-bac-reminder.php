0486d206cd0e0a2a3c42b9395c071c611b7daab7
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-04-02 19:04:11 +0200

    git init bbb-bac-reminder

---
+++ bbb-bac-reminder.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: BigBoxBerlin ÜberweisungsErinnerer
+Plugin URI: https://
+Description: BigBoxBerlin ÜberweisungsErinnerer
+Version: 0.1.0
+Author: Kevin Fechner
+Author URI: https://complete-webolutions.de
+Text Domain: bbb-bac-reminder
+Domain Path: /languages
+ */
+
+if (! defined('ABSPATH')) exit;
+
+define('BBB_BAC_IMPORTER_PATH', WP_PLUGIN_DIR . '/bbb-bac-reminder/');
