3a6e6c11227d230cefe4bbb9df5d5de3d8f0e59e
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-04-02 21:59:09 +0200

    options markup

---
+++ options-page.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+$bminder_days_to_send_reminder = get_option('bminder_days_to_send_reminder', 7);
+$bminder_days_to_update_status = get_option('bminder_days_to_update_status', 14);
+
+?>
+
+<form action="<?php echo home_url() ?>/wp-admin/admin-post.php" method="POST">
+    <input type="hidden" name="action" value="bbb_reminder_options">
+
+    <div>
+        <label for="days_to_send_reminder"><?php _e('Tage bis zur Erinnerungsmail:'); ?></label>
+        <input type="number" name="bminder_days_to_send_reminder" id="days_to_send_reminder" value="<?php echo $bminder_days_to_send_reminder; ?>">
+    </div>
+
+    <div>
+        <label for="days_to_update_status"><?php _e('Tage bis zur automatischen Status Änderung:') ?></label>
+        <input type="number" name="bminder_days_to_update_status" id="days_to_update_status" value="<?php echo $bminder_days_to_update_status; ?>">
+    </div>
+
+
+    <?php submit_button(); ?>
+</form>
