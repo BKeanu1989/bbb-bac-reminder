d3a850be325e6af46f81b19165b2510d0b5640f1
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-04-11 14:44:22 +0200

    add docs, + i18n for update note

--- class-bac-reminder-update-status.php
+++ class-bac-reminder-update-status.php
@@ -1,6 +1,10 @@
 <?php
 
 class BacReminderUpdateStatus {
+    /**
+     * sets an array of orders_to_update
+     * @param string $date_to_update_status | formatted string date
+     */
     public function __construct($date_to_update_status)
     {
         global $wpdb;
@@ -10,6 +14,11 @@ class BacReminderUpdateStatus {
         $this->orders_to_update = array_column($this->orders_to_update, 'ID');
     }
 
+    /**
+     * inits the main handler
+     * foreach order_id in orders_to_update array -> call handle_main
+     * @uses handle_main
+     */
     public function init() 
     {
         foreach ($this->orders_to_update as $order_id) {
@@ -17,6 +26,10 @@ class BacReminderUpdateStatus {
         }
     }
 
+    /**
+     * handles necessary function calls per $order_id
+     * @param int $order_id
+     */
     public function handle_main($order_id)
     {
         $order_id = (int) $order_id;
@@ -24,10 +37,13 @@ class BacReminderUpdateStatus {
         $_order = new WC_Order($order_id);
         $success = $this->update_status($_order);
     }
-
+    /**
+     * updates an order with a comment
+     * @param object $_order | order object of wc
+     */
     public function update_status($_order)
     {
-        $note = __('Status via reminder plugin automatisch geändert');
+        $note = __('Status via reminder plugin automatisch geändert', 'bbb-bac-reminder');
 
         return $_order->update_status('wc-cancelled', $note);
     }
