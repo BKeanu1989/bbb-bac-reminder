fba721ef2cc7e815fb36a3022c4aa37ae1a48c66
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-04-11 13:47:31 +0200

    add docs, update classname

--- class-bac-reminder.php
+++ class-bac-reminder.php
@@ -20,6 +20,12 @@ trait Time_Helpers
 
 class BacReminder {
     use Time_Helpers;
+
+    /**
+     * sets options and if not set, set default values
+     * 
+     * sets dates for send reminder emails & orders to update date
+     */
     public function __construct() 
     {
         $this->days_to_send_reminder = get_option('bminder_days_to_send_reminder', 7);
@@ -28,19 +34,34 @@ class BacReminder {
         $this->date_to_send_reminder = $this->modify_date("-{$this->days_to_send_reminder} days");
         $this->date_to_update_status = $this->modify_date("-{$this->days_to_update_status} days");
     }
-
+    /**
+     * inits main work
+     * 1. update orders
+     * 2. send reminder email for remaining orders
+     * 
+     * @uses handle_orders_to_update, handle_orders_to_resend_email
+     * @return void
+     */
     public function init() 
     {
         $this->handle_orders_to_update();
         $this->handle_orders_to_resend_email();
     }
-    
+
+    /**
+     * updates orders
+     * @uses BacReminderUpdateStatus
+     */
     public function handle_orders_to_update()
     {
-        $orders_to_update = new BacReminderUpdatestatus($this->date_to_update_status);
+        $orders_to_update = new BacReminderUpdateStatus($this->date_to_update_status);
         $orders_to_update->init();
     }
-    
+    /**
+     * sends reminder emails
+     * 
+     * @uses BacReminderResendEmails
+     */
     public function handle_orders_to_resend_email()
     {
         $orders_to_resend_emails = new BacReminderResendEmails($this->date_to_send_reminder);
