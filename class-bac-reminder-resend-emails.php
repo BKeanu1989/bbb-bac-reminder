6da3cfc6d2dfbd72ba4276a0927e598bec8b5456
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-04-11 13:36:07 +0200

    add_email_sent_meta & add documentation

--- class-bac-reminder-resend-emails.php
+++ class-bac-reminder-resend-emails.php
@@ -1,6 +1,11 @@
 <?php
 
 class BacReminderResendEmails {
+    /**
+     * sets an array of orders_to_resend_email
+     * 
+     * @param string $date formatted date string
+     */
     public function __construct($date)
     {
         global $wpdb;
@@ -9,24 +14,43 @@ class BacReminderResendEmails {
         $this->orders_to_resend_email = $wpdb->get_results($wpdb->prepare("SELECT ID FROM {$wpdb->prefix}posts WHERE DATE(post_date) < %s AND post_type = 'shop_order' AND post_status = 'wc-on-hold'", $this->date), ARRAY_A);
         $this->orders_to_resend_email = array_column($this->orders_to_resend_email, 'ID');
     }
-
+    /**
+     * inits the resend email handler
+     * foreach order_id call handle_main(order_id)
+     * 
+     * 
+     * @uses handle_main
+     * @return void
+     */
     public function init() 
     {
         foreach ($this->orders_to_resend_email as $order_id) {
-            $this->handle_main($order_id);
+            $reminder_sent = get_post_meta($order_id, 'reminder_email_sent', true);
+            if (empty($reminder_sent) || $reminder_sent === 'no') {
+                $this->handle_main($order_id);
+            }
         }
     }
-
+    /**
+     * does all the main work for an order_id
+     * @param int $order_id
+     * @uses resend_email, add_comment, add_email_sent_meta
+     */
     public function handle_main($order_id)
     {
         $order_id = (int) $order_id;
         $_order = new WC_Order($order_id);
-// wc_get_order($order_id);
+
         $success = $this->resend_email($_order);
 
         $this->add_comment($_order);
+        $this->add_email_sent_meta($_order);
     }
 
+    /**
+     * triggers reminder email for given order
+     * @param object $order
+     */
     public function resend_email($order)
     {
         $wc_emails = WC()->mailer()->get_emails();
@@ -42,9 +66,25 @@ class BacReminderResendEmails {
         }
     }
 
+    /**
+     * adds an order note after sending an email
+     * @param object $order
+     */
     public function add_comment($order)
     {
-        $note = __('Erinnerungsmail gesendet');
+        $note = __('Erinnerungsmail gesendet', 'bbb-bac-reminder');
         // $order->add_order_note($note);
+    }
+
+    /**
+     * adds 'reminder_email_sent' meta to an order
+     * @param object $order
+     */
+    public function add_email_sent_meta($order) 
+    {
+        $order_id = $order->get_id();
+        // if (!add_post_meta($order_id, 'reminder_email_sent', 'yes', true)) {
+        //     update_post_meta($order_id, 'reminder_email_sent', 'yes');
+        // }
     }
 }
